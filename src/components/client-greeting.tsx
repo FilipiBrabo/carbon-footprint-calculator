7d84d07bdc9c88a92a303460879a54476e6a16af
Filipi Brabo <filipi.brabo@gmail.com>
2025-09-11 22:02:16 -0300

    feat: setup shadcn

--- src/components/client-greeting.tsx
+++ src/components/client-greeting.tsx
@@ -1,11 +1,24 @@
 "use client";
 
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { useTRPC } from "@/trpc/client";
+import { Button } from "./ui/button";
 
 export function ClientGreeting() {
   const trpc = useTRPC();
   const greeting = useSuspenseQuery(trpc.hello.queryOptions({ text: "world" }));
 
-  return <div>{greeting.data.greeting}</div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      {greeting.data.greeting}
+
+      <div>
+        <Button onClick={() => setCount(count + 1)}>Increment</Button>
+        <p>Count: {count}</p>
+      </div>
+    </div>
+  );
 }
