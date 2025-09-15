78659c7216d7e10aca6de9286db2f7d19561f94c
Filipi Brabo <filipi.brabo@gmail.com>
2025-09-15 11:16:16 -0300

    fix: add missing suspense boundary

--- src/app/footprint-calculator/layout.tsx
+++ src/app/footprint-calculator/layout.tsx
@@ -1,3 +1,5 @@
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 import { FootprintCalculatorStoreProvider } from "@/providers/footprint-calculator-store-provider";
 
 export default function Layout({
@@ -7,7 +9,17 @@ export default function Layout({
     <div className="md:max-w-lg mx-auto space-y-4 p-4">
       <h1 className="text-2xl font-bold">Carbon Footprint Calculator</h1>
       <FootprintCalculatorStoreProvider>
-        {children}
+        <Suspense
+          // TODO: better loading state UI
+          // We need suspense here since we use `useSearchParams` in the page
+          fallback={
+            <div className="w-full h-full flex items-center justify-center mx-auto my-auto">
+              <Loader2 className="size-6 animate-spin" />
+            </div>
+          }
+        >
+          {children}
+        </Suspense>
       </FootprintCalculatorStoreProvider>
     </div>
   );
